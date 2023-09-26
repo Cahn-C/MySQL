be80a31bd9a886aaf1577d2fc0db2421aced8049
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-26 03:30:38 -0400

    Update Ecommerce and Web Analystics queries.sql

--- Advanced SQL - MySQL for Ecommerce and Web Analystics/Ecommerce and Web Analystics queries.sql
+++ Advanced SQL - MySQL for Ecommerce and Web Analystics/Ecommerce and Web Analystics queries.sql
@@ -7,3 +7,16 @@ from website_sessions
 where created_at < '2012-04-12'
 group by utm_source, utm_campaign, http_referer
 order by 4 desc;
+
+
+-- Traffic Conversion Rates
+select count(distinct ws.website_session_id) as sessions, 
+	   count(distinct o.order_id) as orders,
+       count(distinct o.order_id) / count(distinct ws.website_session_id) as conversion_rate
+from website_sessions ws
+left join orders o
+on ws.website_session_id = o.website_session_id
+where ws.created_at < '2012-04-14'
+and utm_source = 'gsearch'
+and utm_campaign = 'nonbrand'
+and http_referer = 'https://www.gsearch.com';
