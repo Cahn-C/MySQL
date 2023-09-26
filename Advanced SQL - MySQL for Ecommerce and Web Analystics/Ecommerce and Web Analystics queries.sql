bcc0177c5d0cac86a5e0a16308f06bf32b7a3389
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-26 03:36:32 -0400

    Update Ecommerce and Web Analystics queries.sql

--- Advanced SQL - MySQL for Ecommerce and Web Analystics/Ecommerce and Web Analystics queries.sql
+++ Advanced SQL - MySQL for Ecommerce and Web Analystics/Ecommerce and Web Analystics queries.sql
@@ -10,6 +10,7 @@ order by 4 desc;
 
 
 -- Traffic Conversion Rates
+-- Since the conversion rate is below 4%, I will need to monitor the impact of bid reductions along with analyzing the performance trending by device type in order to refine bidding stategy
 select count(distinct ws.website_session_id) as sessions, 
        count(distinct o.order_id) as orders,
        count(distinct o.order_id) / count(distinct ws.website_session_id) as conversion_rate
