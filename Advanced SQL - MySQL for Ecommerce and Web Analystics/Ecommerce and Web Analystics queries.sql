9a6cc6a9f3c6629c097d377f77d1bc67884a24b7
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-26 04:13:27 -0400

    Update Ecommerce and Web Analystics queries.sql

--- Advanced SQL - MySQL for Ecommerce and Web Analystics/Ecommerce and Web Analystics queries.sql
+++ Advanced SQL - MySQL for Ecommerce and Web Analystics/Ecommerce and Web Analystics queries.sql
@@ -21,3 +21,16 @@ where ws.created_at < '2012-04-14'
 and ws.utm_source = 'gsearch'
 and ws.utm_campaign = 'nonbrand'
 and ws.http_referer = 'https://www.gsearch.com';
+
+
+-- Traffic Source Trending
+-- It seems that gsearch nonbrand is fairly sensitive to bid changes, the company wants to maximize volume, but does not want to spend more on ads than they can afford
+-- Tom will follow up with me shortly
+select min(date(created_at)) as week_start_date, 
+	count(distinct ws.website_session_id) as sessions
+from website_sessions ws
+where created_at < '2012-05-10'
+and ws.utm_source = 'gsearch'
+and utm_campaign = 'nonbrand'
+group by week(created_at)
+order by 1;
