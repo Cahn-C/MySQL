0d94fbc7916adb7f1d2165117026e190127f38ad
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-10-02 02:02:14 -0400

    Update Ecommerce and Web Analystics queries.sql

--- Advanced SQL - MySQL for Ecommerce and Web Analystics/Ecommerce and Web Analystics queries.sql
+++ Advanced SQL - MySQL for Ecommerce and Web Analystics/Ecommerce and Web Analystics queries.sql
@@ -2,7 +2,10 @@ use mavenfuzzyfactory;
 
 -- Finding top Traffic Sources
 -- "gsearch nonbrand" has the largest session volume, we will need to focus more on this campaign to gain more insight for potential optimization opportunities
-select utm_source, utm_campaign, http_referer, count(distinct website_session_id) as sessions  
+select utm_source, 
+       utm_campaign, 
+       http_referer, 
+       count(distinct website_session_id) as sessions  
 from website_sessions
 where created_at < '2012-04-12'
 group by utm_source, utm_campaign, http_referer
