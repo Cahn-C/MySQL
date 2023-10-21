6cb49e4a1caec6b2c1a47a59b3c78acca4226b4e
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-10-21 01:42:49 -0400

    Update Maven_Movies_DVD_Rentals.sql

--- Projects/Maven_Movies_DVD_Rentals.sql
+++ Projects/Maven_Movies_DVD_Rentals.sql
@@ -15,14 +15,14 @@ SELECT * FROM mavenmoviesmini.inventory_non_normalized;
 */
 CREATE TABLE inventory
 SELECT inventory_id,
-	   film_id,
+       film_id,
        store_id
 FROM mavenmoviesmini.inventory_non_normalized;
 
 CREATE TABLE films
 SELECT DISTINCT
-	   film_id,
-	   title,
+       film_id,
+       title,
        description,
        release_year,
        rental_rate,
@@ -31,8 +31,8 @@ FROM mavenmoviesmini.inventory_non_normalized;
 
 CREATE TABLE stores
 SELECT DISTINCT 
-	   store_id,
-	   store_manager_first_name,
+       store_id,
+       store_manager_first_name,
        store_manager_last_name,
        store_address,
        store_city,
